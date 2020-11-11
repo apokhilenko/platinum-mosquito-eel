1c259106c74e243cb2e1655198bc622b8ae1fa3e
Anton Pokhylenko <anton.pokhilenko@gmail.com>
2020-11-11 16:13:50 +0200

    Add initial setup and cleanup

---
+++ src/theme/GlobalStyle.tsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        font-family: Arial,Helvetica,sans-serif;
+    }
+
+    html {
+        font-size: 1rem;
+    }
+
+    html, body, #root {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+`;
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+export const theme = {
+  mainColor: "#007edb",
+  whiteColor: "#fff",
+};
+
+export { GlobalStyle } from "./GlobalStyle";
